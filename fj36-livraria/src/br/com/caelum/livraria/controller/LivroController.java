586fbd2c21e7ec21a92baa230c75e46ae7277975
marcelocurtis <marcelocurtis@fe80:0:0:0:2421:b6ff:fe29:3f17%9>
2017-08-31 08:53:20 -0300

    30/08/2017

---
+++ fj36-livraria/src/br/com/caelum/livraria/controller/LivroController.java
@@ -0,0 +1,21 @@
+package br.com.caelum.livraria.controller;
+
+//@Controller
+//@RequestMapping("/livros")
+public class LivroController {
+//
+//	@RequestMapping(value="/", method="GET")
+//	public List<Livro> lista (){
+//		return null;
+//	}
+//	
+//	@RequestMapping(value="/{id}", method="GET")
+//	public ResponseEntity<Livro> buscaPorId(@PathVariable("id") Integer id){
+//		
+//	}
+//	
+//	@RequestMapping(value="/", method="SALVAR")
+//	public ResponseEntity<Livro> salvar(@RequestBody Livro livro){
+//		
+//	}
+}
---
+++ fj36-webservice/src/br/com/caelum/payfast/rest/PagamentoService.java
@@ -0,0 +1,11 @@
+package br.com.caelum.payfast.rest;
+
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class PagamentoService extends Application {
+	
+}
